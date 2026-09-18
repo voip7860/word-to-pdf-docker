0faeb2e2e65e5f28af9cca0c364ab572e470fb4e
voip7860 <voip7860@gmail.com>
2026-09-18 11:25:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# یہاں اپنی CloudConvert کی API Key پیسٹ کریں
-CLOUDCONVERT_API_KEY = "YOUR_CLOUDCONVERT_API_KEY_HERE"
+# CloudConvert API Key integrated
+CLOUDCONVERT_API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTBjMzYxNGM2M2FkM2RmYWUyMWIyNDI3YmI2MGZjMmU4MjNjOWUxZjQyOGUyNTNmNDE4NmY4MmE0ZTUwYWIxNmNhOWM4ZDczNGQ1OWMyZDgiLCJpYXQiOjE3ODk2NzAzNzEuMDUwMzA1LCJuYmYiOjE3ODk2NzAzNzEuMDUwMzA3LCJleHAiOjQ5NDUzNDM5NzEuMDQxNjc1LCJzdWIiOiI3NzAwODExNCIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ0YXNrLnJlYWQiLCJ0YXNrLndyaXRlIl19.VC3egQDirvbmCSKWBqZSeNxuQxzakS49AtosMIYlb6FLc94_No4SZrs5i_VbC3kpwhHt1cpCI63ItrPEiuUsEzt_VdnsXSetyONSgFgzfikvPgjVyb3wC08dXqVEcpRiDUEpfCT0TQlbRSfCSTrwvOOI7JwuDH4Ow1JAk3F9wTsCBxRBbxStlfVUM0NkqodbvitiDaksQymGwAmUNT12wIIGWFe0vWQPz-SxEjGqrFxZDEg7heLMDWR4O6c2t2Zy1i140TB_3OSj_Lkf-XuoCsXkQc9DhlPscrsT0Efk9_qQy3SQhUCwlvMQskpezV-r7y1Na_LTNm-WUEVy96fwM20AJKZesztTszGx9KEEkSyebVzxisMHo971o7QZsliQc1AfntHS0hTCeFlB4IidvwzYrCCdMXOdDK2fiJ2ExgRxd311AZ13EuZl99v0gtEiVp8HnRIWLQ_E3YypP-tZKUE_FYsRhkxfII3PIl0Sue8qcFAt0UqyyoPbm1DIpLk5iwdTElDoZo1c9jpxo7I-FUrjxYwTQbwkARl8B-os0IZBkCGQmCfSzakK9p4usnRoCKY9mHs-p5idycUPP_P-As4YZxiE0dFkXe8L0j_AjbXXRKF8S19-GJ7kzg1o4C6TXZz_NSlnKk1YEB6APOZ0ONEW_Om7Pb_-DNF-R9uplPI"
 
 cloudconvert.configure(api_key=CLOUDCONVERT_API_KEY)
 
@@ -32,7 +32,7 @@ def convert_word_to_pdf():
         
         file.save(docx_path)
         
-        # CloudConvert Job Creation for MS Word exact rendering
+        # CloudConvert Job Creation using MS Office Engine
         job = cloudconvert.Job.create(payload={
             "tasks": {
                 "import-my-file": {
@@ -55,12 +55,11 @@ def convert_word_to_pdf():
         upload_task = [task for task in job['tasks'] if task['name'] == 'import-my-file'][0]
         cloudconvert.Task.upload(file_name=docx_path, task=upload_task)
         
-        # Wait for conversion to finish
+        # Wait for conversion to complete
         cloudconvert.Job.wait(id=job['id'])
         
         # Download converted PDF
         export_task = [task for task in job['tasks'] if task['name'] == 'export-my-file'][0]
-        file_info = export_task['result']['files'][0]
         cloudconvert.Task.download(file_name=pdf_path, task=export_task)
         
         if os.path.exists(pdf_path):
